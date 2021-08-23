527d3a4cdf4fe03a52e38658f309a115c298952f
Awet9I <s362104@oslomet.no>
2021-08-23 17:19:39 +0200

    second student added a minimum finder function

--- src/Example01.java
+++ src/Example01.java
@@ -13,10 +13,24 @@ public class Example01 {
 
     }
 
+    public static int miniFinder(int [] b){
+        int maxValue = 0;
+
+        for(int i = 0; i < b.length; i++){
+            if(b[i] < b[maxValue]){
+                maxValue = i;
+            }
+        }
+        return maxValue;
+    }
+
     public static void main(String[] args) {
         int [] a = {12, 1, 32, 8, 9, 11, 43, 84, 3};
 
        int i =  findMax(a);
         System.out.println(i);
+
+        int n = miniFinder(a);
+        System.out.println(n);
     }
 }
